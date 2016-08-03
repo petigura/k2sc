4d0a7b41750d2a934e8c2227d26fc8500c5bd7e3
Erik Petigura <petigura@caltech.edu>
2016-08-03 16:14:32 -0700

    hard-coding in use of basic kernel

--- src/k2sc.py
+++ src/k2sc.py
@@ -157,6 +157,7 @@ def detrend(dataset, args):
         
     ## Kernel selection
     ## ----------------
+    args.kernel='basic'
     if args.kernel:
         info('Overriding automatic kernel selection, using %s kernel as given in the command line', args.kernel)
         if 'periodic' in args.kernel and not args.kernel_period:
